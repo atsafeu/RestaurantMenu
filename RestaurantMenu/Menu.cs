016a6d627970028fb13c4868b6abb65d52547cd2
Armand Tsafack <armandtsafack@armandtsafack-2.home>
2020-10-13 21:00:29 -0500

    add Restaurant

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace RestaurantMenu
+{
+    public class Menu
+    {
+  
+        public DateTime LastUpdated { get; }
+        public List<MenuItem> Items { get; set; }
+
+        public Menu(DateTime dateUpdated, List<MenuItem> item)
+        {
+            LastUpdated = dateUpdated;
+            Items = item;
+        }
+    }
+    
+}
---
+++ RestaurantMenu/MenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace RestaurantMenu
+{
+    public class MenuItem
+    {
+     public double Price { get; set; }
+     public string Description { get; set; }
+     public string Category { get; set; }
+    public bool isNew { get; set; }
+
+        public MenuItem(double price, string description, string category, bool isNew);
+        Price=price;
+        Description=description;
+        Category=category;
+        IsNew=isNew;
+
+
+    }
+}
